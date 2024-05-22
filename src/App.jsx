1d1ad00264f70fb68040019cb039625f1f326340
Majidkhan3 <majidabid017@gmail.com>
2024-05-22 18:59:04 +0500

    adding-elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-
-
-import Header from './components/Header';
-import Hero from './components/Hero';
+import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App() {
   return (
-    <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-
+    <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden new-class">
       <Header />
       <Hero />
-
     </div>
   );
 }
