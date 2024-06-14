11ab0674659cd9d5547890590a5db49964067397
Majidkhan3 <majidabid017@gmail.com>
2024-06-14 23:21:20 +0500

    changes

--- src/components/Brain.jsx
+++ src/components/Brain.jsx
@@ -1,11 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 const Brain = () => {
+  const [open, SetOpen] = useState(false);
+  function OpenSideBar(prev) {
+    SetOpen(() => prev(!prev));
+  }
   useEffect(() => {
     const url = fetch(`www.org.com`);
     url.reponse(this(() => {}));
     return;
   }, []);
-  return <div>Brain</div>;
+  return <div onClick={OpenSideBar}>Brain</div>;
 };
 
 export default Brain;
+9;
