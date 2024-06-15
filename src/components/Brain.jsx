87c1750c86347681ba29259d24415f79eadcf6dd
Majidkhan3 <majidabid017@gmail.com>
2024-06-15 22:25:06 +0500

    array maping system

--- src/components/Brain.jsx
+++ src/components/Brain.jsx
@@ -1,9 +1,22 @@
 import React, { useEffect, useState } from "react";
-const Brain = () => {
+const newarr = {
+  age: "Majid",
+  name: "",
+};
+const anotheraArr = { ...newarr };
+
+anotheraArr.age = "Majid";
+anotheraArr.name = "abid";
+
+const Brain = ({ ...props }) => {
+  const { setState, setMyState } = props;
   const [open, SetOpen] = useState(false);
   function OpenSideBar(prev) {
     SetOpen(() => prev(!prev));
   }
+  const increment = () => {
+    SetOpen(open + 1);
+  };
   useEffect(() => {
     const url = fetch(`www.org.com`);
     url.reponse(this(() => {}));
